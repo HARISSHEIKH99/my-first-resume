705df29e20453800377f511afe167865209e43ac
AyeshaHaris <ayeshaharis1999@gmail.com>
2024-09-08 00:36:04 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var nameElement = document.getElementById("name");
+var contactElement = document.getElementById("contact");
+var user = {
+    name: "Ayesha Haris",
+    contact: "ayeshaharis1999@gamil.com | (123) 456-7890"
+};
+nameElement.textContent = user.name;
+contactElement.textContent = user.contact;
---
+++ script.ts
@@ -0,0 +1,9 @@
+const nameElement = document.getElementById("name");
+const contactElement = document.getElementById("contact");
+
+const user = {
+    name: "Ayesha Haris",
+    contact: "ayeshaharis1999@gamil.com | (123) 456-7890"};
+
+    nameElement! .textContent = user.name;
+    contactElement!.textContent = user.contact;
